c3657e3202c5d1b7b1ae278a694bf32f3e371c90
dtopalov <dimiter.topalov@gmail.com>
2017-04-25 15:36:27 +0300

    chore: Add intl support configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
@@ -7,6 +7,28 @@ import { AppComponent } from './app.component';
 
 // Import the ButtonsModule...
 import { ButtonsModule } from '@progress/kendo-angular-buttons';
+
+import { load, IntlModule } from '@progress/kendo-angular-intl';
+
+import * as likelySubtags from 'cldr-data/supplemental/likelySubtags.json';
+import * as weekData from 'cldr-data/supplemental/weekData.json';
+import * as currencyData from 'cldr-data/supplemental/currencyData.json';
+import * as numbers from 'cldr-data/main/de/numbers.json';
+import * as timeZoneNames from 'cldr-data/main/de/timeZoneNames.json';
+import * as calendar from 'cldr-data/main/de/ca-gregorian.json';
+import * as currencies from 'cldr-data/main/de/currencies.json';
+import * as dateFields from 'cldr-data/main/de/dateFields.json';
+
+load(
+    likelySubtags,
+    weekData,
+    currencyData,
+    numbers,
+    currencies,
+    calendar,
+    dateFields,
+    timeZoneNames
+);
 
 @NgModule({
   declarations: [
@@ -16,11 +38,14 @@ import { ButtonsModule } from '@progress/kendo-angular-buttons';
     BrowserModule,
     FormsModule,
     HttpModule,
+    IntlModule,
 
     // ... and register it
     ButtonsModule
   ],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'de-DE' }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/typings.d.ts
+++ src/typings.d.ts
@@ -3,3 +3,9 @@ declare var module: NodeModule;
 interface NodeModule {
   id: string;
 }
+
+/* Enable importing from .json files */
+declare module "*.json" {
+    const value: any;
+    export default value;
+}
