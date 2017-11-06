8a1d12ee4f8e443d5008bb369f109fff417a56d8
Tsvetomir Tsonev <cvetomirconev@gmail.com>
2017-11-06 12:37:48 +0200

    feat: upgrade to Angular 5.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
@@ -8,32 +9,13 @@ import { AppComponent } from './app.component';
 // Import the ButtonsModule...
 import { ButtonsModule } from '@progress/kendo-angular-buttons';
 
-/* Loading CLDR data
+/* Loading CLDR data, see
  * http://www.telerik.com/kendo-angular-ui/components/internationalization/
+ *
+ * // Load all required data for the de locale
+ * import '@progress/kendo-angular-intl/locales/de/all';
+ */
 
-import { load } from '@progress/kendo-angular-intl';
-
-import * as likelySubtags from 'cldr-data/supplemental/likelySubtags.json';
-import * as weekData from 'cldr-data/supplemental/weekData.json';
-import * as currencyData from 'cldr-data/supplemental/currencyData.json';
-import * as numbers from 'cldr-data/main/de/numbers.json';
-import * as timeZoneNames from 'cldr-data/main/de/timeZoneNames.json';
-import * as calendar from 'cldr-data/main/de/ca-gregorian.json';
-import * as currencies from 'cldr-data/main/de/currencies.json';
-import * as dateFields from 'cldr-data/main/de/dateFields.json';
-
-load(
-    likelySubtags,
-    weekData,
-    currencyData,
-    numbers,
-    currencies,
-    calendar,
-    dateFields,
-    timeZoneNames
-);
-
-*/
 
 @NgModule({
   declarations: [
@@ -41,6 +23,7 @@ load(
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
     HttpModule,
 
