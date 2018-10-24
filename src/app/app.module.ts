3edec4b27f73959d41222938ca66b7208f6e690a
dtopalov <dimiter.topalov79@gmail.com>
2018-10-24 15:00:08 +0300

    chore: upgrade to Angular 7

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-
-// Import the ButtonsModule...
 import { ButtonsModule } from '@progress/kendo-angular-buttons';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-/* Loading CLDR data, see
- * http://www.telerik.com/kendo-angular-ui/components/internationalization/
- *
- * // Load all required data for the de locale
- * import '@progress/kendo-angular-intl/locales/de/all';
- */
 
 
 @NgModule({
@@ -23,13 +13,10 @@ import { ButtonsModule } from '@progress/kendo-angular-buttons';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule,
-    FormsModule,
-    HttpModule,
-
-    // ... and register it
-    ButtonsModule
+    ButtonsModule,
+    BrowserAnimationsModule
   ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
